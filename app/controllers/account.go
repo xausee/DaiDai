8136d3b5df40c1d6b4dc4814d69d1251aeef68f1
xausee <xausee@gmail.com>
2013-10-28 01:59:10 -0700

    update account

--- app/controllers/account.go
+++ app/controllers/account.go
@@ -13,6 +13,37 @@ func (c *Account) Register() revel.Result {
   return c.Render() 
 }
 
+// func (c *Account) PostRegister(user *models.MockUser) revel.Result { 
+//   return c.Render() 
+// }
+
 func (c *Account) PostRegister(user *models.MockUser) revel.Result { 
+  c.Validation.Email(user.Email).Message("电子邮件格式无效") 
+  c.Validation.Required(user.Nickname).Message("用户昵称不能为空") 
+  c.Validation.Required(user.Password).Message("密码不能为空") 
+  c.Validation.Required(user.ConfirmPassword == user.Password).Message("两次输入的密码不一致")
+
+  if c.Validation.HasErrors() { 
+    c.FlashParams() 
+    return c.Redirect((*Account).Register) 
+  }
+
+  dal, err := models.NewDal() 
+  if err != nil { 
+    c.Response.Status = 500 
+    return c.RenderError(err) 
+  } 
+  defer dal.Close()
+
+  err = dal.RegisterUser(user) 
+  if err != nil { 
+    c.Flash.Error(err.Error()) 
+    return c.Redirect((*Account).Register) 
+  }
+
+  return c.Redirect((*Account).RegisterSuccessful) 
+}
+
+func (c *Account) RegisterSuccessful() revel.Result { 
   return c.Render() 
 }
