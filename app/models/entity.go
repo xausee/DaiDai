ed80fac83c0162443c6a87ed3549dc01a46ad752
ubuntu11 <xausee@ubuntu.(none)>
2013-11-03 08:17:37 -0800

    align the string

--- app/models/entity.go
+++ app/models/entity.go
@@ -3,14 +3,14 @@ package models
 type User struct { 
   Email    string 
   Nickname string 
-  Gender      string
+  Gender   string
   Password []byte 
 }
 
 type MockUser struct { 
   Email           string 
   Nickname        string 
-  Gender             string
+  Gender          string
   Password        string 
   ConfirmPassword string   
 }
