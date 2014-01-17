391fe498ce64a30525f984568c60f32143f811ca
xausee <xausee@gmail.com>
2014-01-17 15:02:49 +0800

    update home page

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -4,7 +4,7 @@ import (
 	"ZhaiLuBaiKe/app/models"
 	"fmt"
 	"github.com/robfig/revel"
-	//"strings"
+	"strings"
 )
 
 type App struct {
@@ -25,7 +25,7 @@ func (c App) Index() revel.Result {
 	aqCount := len(allQuotation)
 	fmt.Println("总共存在", aqCount, "条数据")
 	if aqCount >= 2 {
-		//allQuotation[count-1].Content = strings.Replace(allQuotation[count-1].Content, "\r\n", "<br>", -1)
+		allQuotation[aqCount-1].Content = strings.Replace(allQuotation[aqCount-1].Content, "\r\n", "<br>", -1)
 		c.RenderArgs["quotations1"] = allQuotation[aqCount-1]
 		c.RenderArgs["quotations2"] = allQuotation[aqCount-2]
 	}
@@ -41,19 +41,17 @@ func (c App) Index() revel.Result {
 		c.RenderArgs["witticism4"] = allWitticism[awCount-4]
 	}
 
-	// 获取古诗词最新1条数据
-	aps, _ := manager.GetAllAncientPoem()
-	apCount := len(aps)
-	if apCount >= 1 {
-		c.RenderArgs["ancientPoem1"] = aps[apCount-1]
-	}
+	// 获取古诗词最新15条数据
+	c.RenderAncientPoems(manager)
 
-	// 获取现代诗最新1条数据
-	mps, _ := manager.GetAllModernPoem()
-	mpCount := len(mps)
-	if mpCount >= 1 {
-		c.RenderArgs["modernPoem1"] = mps[mpCount-1]
-	}
+	// 获取现代诗最新15条数据
+	// mps, _ := manager.GetAllModernPoem()
+	// mpCount := len(mps)
+	// if mpCount >= 1 {
+	// 	c.RenderArgs["modernPoem1"] = mps[mpCount-1]
+	// }
+
+	c.RenderModernPoems(manager)
 
 	// 获取散文最新1条数据
 	es, _ := manager.GetAllEssay()
@@ -70,4 +68,30 @@ func (c App) Add() revel.Result {
 	nickName := c.Session["nickName"]
 
 	return c.Render(email, nickName)
+}
+
+func (c *App) RenderAncientPoems(manager *models.DbManager) error {
+	poems, err := manager.GetAllAncientPoem()
+	count := len(poems)
+	if count >= 4 {
+		c.RenderArgs["ancientPoem1"] = poems[count-1]
+		c.RenderArgs["ancientPoem2"] = poems[count-2]
+		c.RenderArgs["ancientPoem3"] = poems[count-3]
+		c.RenderArgs["ancientPoem4"] = poems[count-4]
+		c.RenderArgs["ancientPoem5"] = poems[count-5]
+	}
+	return err
+}
+
+func (c *App) RenderModernPoems(manager *models.DbManager) error {
+	poems, err := manager.GetAllModernPoem()
+	count := len(poems)
+	if count >= 1 {
+		c.RenderArgs["modernPoem1"] = poems[count-1]
+		// c.RenderArgs["modernPoem2"] = poems[count-2]
+		// c.RenderArgs["modernPoem3"] = poems[count-3]
+		// c.RenderArgs["modernPoem4"] = poems[count-4]
+		// c.RenderArgs["modernPoem5"] = poems[count-5]
+	}
+	return err
 }
